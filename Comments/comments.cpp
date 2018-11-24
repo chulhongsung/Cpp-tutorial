c2b343b76b98e1a2c94dd991f5f77cfaae58a310
SungChul Hong <hsc930526@gmail.com>
2018-11-25 03:59:36 +0900

    Create comments.cpp

---
+++ Comments/comments.cpp
@@ -0,0 +1,33 @@
+/*
+	This is my program.
+	Written by 'chulhongsung'.
+*/
+
+#include <iostream> // iostream: input output
+					// include: preprocessor directive
+int main(void)
+{
+	int x = 2; // 2: literal
+	x = 5; 
+	int y = x + 3;
+
+	
+	std::cout << y << std::endl;
+	
+	int sight = 10;
+
+	// ...
+
+	// 마법의 물약을 먹어서 시야가 0
+
+	sight = 0;
+
+	return 0; 
+}
+
+
+
+
+
+
+
